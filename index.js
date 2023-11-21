a287eb37732d4e1629f8730b8460fe813f1b2c67
pablocordonie <pablogarcia.93.06@gmail.com>
2023-11-21 21:33:13 +0100

    solve the next 7 exercises

--- index.js
+++ index.js
@@ -379,70 +379,146 @@ repeatCounter(counterWords);
 
 // Ejercicio 15
 
-// Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta". Usa la función .includes de javascript.
+// Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta".
 
-// const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinturón de Orión', 'AC/DC Camiseta'];
+
+const shirtsFilter = (array) => {
+  const shirts = [];
+
+  array.forEach(product => {
+    if (product.includes('Camiseta')) {
+      shirts.push(product);
+    }
+  });
+
+  return shirts.join(', ');
+};
+
+console.log(shirtsFilter(products));
 
 // Ejercicio 16
 
 // Probando For...of: Usa un bucle forof para recorrer todos los destinos del array. Imprime en un console.log sus valores. Puedes usar este array:
 
-// const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+
+for (const place of placesToTravel) {
+  console.log(place);
+}
 
 // Ejercicio 17
 
-// Probando For...in: Usa un for...in para imprimir por consola los datos del alienígena.. Puedes usar este objeto:
+// Probando For...in: Usa un for...in para imprimir por consola los datos del alienígena... Puedes usar este objeto:
 
-/* const alien = {
+const alien = {
   name: 'Wormuck',
   race: 'Cucusumusu',
   planet: 'Eden',
   weight: '259kg'
-}; */
+};
+
+for (const prop in alien) {
+  console.log(prop + ': ' + alien[prop]);
+}
 
 // Ejercicio 18
 
 // Probando For: Usa un bucle for para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40. Imprime en un console log el array. Puedes usar este array:
 
-// const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}];
+const placesToTravel_2 = [{ id: 5, name: 'Japan' }, { id: 11, name: 'Venecia' }, { id: 23, name: 'Murcia' }, { id: 40, name: 'Santander' }, { id: 44, name: 'Filipinas' }, { id: 59, name: 'Madagascar' }];
+
+for (let i = 0; i < placesToTravel_2.length; i++) {
+  const place = placesToTravel_2[i];
+  for (const prop in place) {
+    if (place[prop] === 11 || place[prop] === 40) {
+      placesToTravel_2.splice(i, 1);
+    }
+  }
+}
+
+console.log(placesToTravel_2);
 
 // Ejercicio 19
 
-// Mixed For e includes: Usa un bucle for para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Recuerda que puedes usar la función .includes() para comprobarlo. Puedes usar este array:
+// Mixed For e includes: Usa un bucle for para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Puedes usar este array:
 
-/* const toys = [
-  {id: 5, name: 'Buzz MyYear'},
-  {id: 11, name: 'Action Woman'},
-  {id: 23, name: 'Barbie Man'},
-  {id: 40, name: 'El gato con Guantes'},
-  {id: 40, name: 'El gato felix'}
-]; */
+const toys = [
+  { id: 5, name: 'Buzz MyYear' },
+  { id: 11, name: 'Action Woman' },
+  { id: 23, name: 'Barbie Man' },
+  { id: 40, name: 'El gato con Guantes' },
+  { id: 40, name: 'El gato felix' }
+];
+
+for (const toy of toys) {
+  if (toy.name.includes('gato')) {
+    toys.splice(toys.indexOf(toy));
+  }
+}
+
+console.log(toys);
 
 // Ejercicio 20
 
 // For...of avanzado: Usa un bucle for...of para recorrer todos los juguetes y añade los que tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola.. Puedes usar este array:
 
-// const popularToys = [];
+const popularToys = [];
 
-/* const toys = [
-  {id: 5, name: 'Buzz MyYear', sellCount: 10},
-  {id: 11, name: 'Action Woman', sellCount: 24},
-  {id: 23, name: 'Barbie Man', sellCount: 15},
-  {id: 40, name: 'El gato con Guantes', sellCount: 8},
-  {id: 40, name: 'El gato felix', sellCount: 35}
-]; */
+const toys_2 = [
+  { id: 5, name: 'Buzz MyYear', sellCount: 10 },
+  { id: 11, name: 'Action Woman', sellCount: 24 },
+  { id: 23, name: 'Barbie Man', sellCount: 15 },
+  { id: 40, name: 'El gato con Guantes', sellCount: 8 },
+  { id: 40, name: 'El gato felix', sellCount: 35 }
+];
+
+const toysFilterBySales = (array) => {
+  for (const toy of array) {
+    if (toy.sellCount > 15) {
+      popularToys.push(toy);
+      console.log(`${toy.name} ha conseguido más de 15 ventas, con un número total de ${toy.sellCount} ventas`);
+    }
+  }
+  return popularToys;
+};
+
+console.log(toysFilterBySales(toys_2));
 
 // Ejercicio 21
 
 // Usa un bucle y dos condiciones para imprimir por consola el nombre de los usuarios que sean menor de edad precedidos del texto "Usuarios menores de edad:" y otro que imprima a los usuarios mayores de edad, precedido del texto "Usuarios mayores de edad:".
 
-/* const users = [
-  { name: "Tony", years: 43 },
-  { name: "Peter", years: 18 },
-  { name: "Natasha", years: 14 },
-  { name: "Bruce", years: 32 },
-  { name: "Khamala", years: 16 },
-]; */
+const users = [
+  { name: "Tony", age: 43 },
+  { name: "Peter", age: 18 },
+  { name: "Natasha", age: 14 },
+  { name: "Bruce", age: 32 },
+  { name: "Khamala", age: 16 },
+];
+
+const underAgeUsersFilter = (array) => {
+  const underAgeUsers = [];
+  array.forEach(user => {
+    if (user.age < 18) {
+      underAgeUsers.push(user);
+    }
+  });
+  return 'Usuarios/as menores de edad: ' + underAgeUsers.map(user => user.name).join(', ');
+};
+
+const adultUsersFilter = (array) => {
+  const adultUsers = [];
+  array.forEach(user => {
+    if (user.age >= 18) {
+      adultUsers.push(user);
+    }
+  });
+  return 'Usuarios/as mayores de edad: ' + adultUsers.map(user => user.name).join(', ');
+}
+
+console.log(underAgeUsersFilter(users));
+console.log(adultUsersFilter(users));
 
 // Ejercicio 22
 
